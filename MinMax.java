e61e5959c096288e01ca9d33fe73193925640815
kokluosman <47574942+kokluosman@users.noreply.github.com>
2022-09-12 12:42:42 +0300

    Min Max

---
+++ MinMax.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+        int loops;
+        System.out.println("Kaç tane tam sayı gireceksiniz?");
+        Scanner scanner = new Scanner(System.in);
+        loops = scanner.nextInt();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 1; i <= loops; i++) {
+            arrayList.add(scanner.nextInt());
+        }
+        int min = arrayList.get(0);
+        int max = arrayList.get(0);
+        System.out.println("------------");
+        System.out.println(arrayList.size());
+        for (int i = 0; i < arrayList.size(); i++) {
+            if(arrayList.get(i)>max){
+                System.out.println(arrayList.get(i));
+                max = i;
+            }
+            if(arrayList.get(i)<min){
+                min = i;
+            }
+        }
+        System.out.println("------------");
+        System.out.println(arrayList);
+        System.out.println("Min Değer:"+min+"\nMax Değer:"+max);
+    }
+}
