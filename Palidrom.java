442ff2253d441b9e3a8c8c7f13efbe4ada66a57f
kokluosman <47574942+kokluosman@users.noreply.github.com>
2022-09-12 18:46:20 +0300

    Fibonacci

---
+++ Palidrom.java
@@ -0,0 +1,23 @@
+public class Palidrom {
+    public static void main(String[] args) {
+        
+        System.out.println(isPalindrom(2002));
+
+    }
+
+    static boolean isPalindrom(int number){
+        int temp = number, reversenumber =0,lastNumber;
+    while(temp!=0){
+        lastNumber = temp %10;
+        reversenumber = (reversenumber*10)+lastNumber;
+        temp/=10;
+    }
+    if(number == reversenumber){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+    }
+}
