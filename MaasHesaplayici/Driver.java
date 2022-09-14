acb968e853f43445be234f10ab8af98df6367c8f
kokluosman <47574942+kokluosman@users.noreply.github.com>
2022-09-14 22:37:49 +0300

    Maas Hesaplayici

---
+++ MaasHesaplayici/Driver.java
@@ -0,0 +1,8 @@
+package MaasHesaplayici;
+
+public class Driver {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Osman", 2000, 45, 36);
+        employee.ttoString();
+    }
+}
