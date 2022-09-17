51ae87c0097d5cdcb558f3c7f3c75787b5b7a73c
kokluosman <47574942+kokluosman@users.noreply.github.com>
2022-09-17 18:01:04 +0300

    MineSwepper

---
+++ MayınTarlasıOyunu/Driver.java
@@ -0,0 +1,19 @@
+package MayınTarlasıOyunu;
+
+import java.util.Scanner;
+
+public class Driver {
+    
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Minesweeper game ! \n" +
+                    "enter the size of the field, for example: (5 4) : ");
+            int x , y ;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            MineSweeper mineSweeper = new MineSweeper(x, y);
+            mineSweeper.run();
+            scanner.close();
+        }
+    
+}
